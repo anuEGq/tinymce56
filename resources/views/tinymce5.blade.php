7ae80a7e60bd8ac50841611b4ae7a3430e6a52b5
anuEGq <anu.1110rvt@gmail.com>
2022-12-27 11:03:12 +0530

    first commit

---
+++ resources/views/tinymce5.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tinymce 5</title>
+    <script src="{{ asset('js/tinymce5/tinymce.min.js') }}" referrerpolicy="origin"></script>
+</head>
+<body>
+    <h1>Tinymce 5</h1>
+    <textarea class="tinymce-editor"></textarea>
+</body>
+<script type="text/javascript">
+    const editor = tinymce.init({
+    selector: '.tinymce-editor',
+    height: 180,
+    menubar: false,
+    toolbar: 'undo redo | formatselect | ' +
+        'bold italic backcolor | alignleft aligncenter ' +
+        'alignright alignjustify | bullist numlist outdent indent | ' +
+        'removeformat | help'
+});
+</script>
+</html>
